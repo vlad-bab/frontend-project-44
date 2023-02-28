42b34e6dc0bacd153d58f313057f014b7693a636
Vladislav Babkin <babkinvladislav01@gmail.com>
2023-03-01 02:37:47 +0500

    add prime game

---
+++ src/games/prime.js
@@ -0,0 +1,27 @@
+import gameEngine from '../index.js';
+import { getRandomNumber } from '/home/vladislavba/frontend-project-44/src/randomNumberGenerator.js';
+
+const isPrime = (number) => {
+    if (number < 2) { return false }
+
+    for (let i = 2; i < number; i += 1) {
+        if (number % i === 0) {
+            return false;
+        }
+        return true;
+        }
+    };
+
+const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const gameLogic = () => {
+    
+    const question = getRandomNumber();
+    const correctAnswer = isPrime(question) ? 'yes' : 'no';
+
+    return [question, correctAnswer];
+};
+
+export default () => {
+    gameEngine(gameDescription, gameLogic);
+};
