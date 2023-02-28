d6ce0bfcfdbaa39492526e78c02f3afd0457b809
Vladislav Babkin <babkinvladislav01@gmail.com>
2023-02-28 23:24:37 +0500

    fix import issue

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import { startCalcGame } from '../src/games/calc.js';
+import startCalcGame from '../src/games/calc.js';
 
 startCalcGame();
--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import { startEvenGame } from '../src/games/even.js';
+import startEvenGame from '../src/games/even.js';
 
 startEvenGame();
