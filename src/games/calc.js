eb7dd3622a50918cf01f49349931596f0321e174
Vladislav Babkin <babkinvladislav01@gmail.com>
2023-02-28 23:30:34 +0500

    step 6

--- src/games/calc.js
+++ src/games/calc.js
@@ -6,8 +6,8 @@ const operators = ['+', '-', '*'];
 const gameDescription = 'What is the result of the expression?';
     
 const gameLogic = () => {
-    const firstNumber = getRandomNumber(1, 10);
-    const secondNumber = getRandomNumber(1, 10);
+    const firstNumber = getRandomNumber(1, 50);
+    const secondNumber = getRandomNumber(1, 50);
     const operator = operators[getRandomNumber(0, operators.length - 1)];
 
     const question = `${firstNumber} ${operator} ${secondNumber}`;
