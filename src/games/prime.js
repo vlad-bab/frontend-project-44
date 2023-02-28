74ffcc8aba706fe831ff2aee0fb4df9d34f8ebc3
Vladislav Babkin <babkinvladislav01@gmail.com>
2023-03-01 03:48:27 +0500

    fix

--- src/games/prime.js
+++ src/games/prime.js
@@ -2,22 +2,22 @@ import gameEngine from '../index.js';
 import getRandomNumber from '../randomNumberGenerator.js';
 
 const isPrime = (number) => {
-    if (number === 2) {
-        return true
+    if (number <= 1) {
+      return false;
     }
-
-    for (let i = 2; i < number; i += 1) {
-        if (number % i === 0) {
-            return false;
-        }
-        return true;
-        }
-    };
+  
+    for (let i = 2; i < Math.sqrt(number); i += 1) {
+      if (number % i === 0) {
+        return false;
+      }
+    }
+  
+    return true;
+};
 
 const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const gameLogic = () => {
-    
     const question = getRandomNumber();
     const correctAnswer = isPrime(question) ? 'yes' : 'no';
 
