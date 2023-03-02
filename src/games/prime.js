074ba7f6598d13fad82728c8d494bc32ef2d446c
Vladislav Babkin <babkinvladislav01@gmail.com>
2023-03-02 13:40:11 +0500

    fix

--- src/games/prime.js
+++ src/games/prime.js
@@ -6,7 +6,7 @@ const isPrime = (number) => {
     return false;
   }
 
-  for (let i = 2; i < Math.sqrt(number); i += 1) {
+  for (let i = 2; i <= Math.sqrt(number); i += 1) {
     if (number % i === 0) {
       return false;
     }
