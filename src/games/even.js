04ce992dd47cd3e0925d6b08a4bcd41ee6e08148
vlad-bab <118886918+vlad-bab@users.noreply.github.com>
2023-05-15 17:18:50 +0500

    Update even.js

--- src/games/even.js
+++ src/games/even.js
@@ -2,10 +2,9 @@ import gameEngine from '../index.js';
 import getRandomNumber from '../randomNumberGenerator.js';
 
 const gameDescription = 'Answer "yes" if the number is even, otherwise answer "no".';
+const isEven = (number) => number % 2 === 0;
 
 const gameLogic = () => {
-  const isEven = (number) => number % 2 === 0;
-
   const question = getRandomNumber();
   const correctAnswer = isEven(question) ? 'yes' : 'no';
 
