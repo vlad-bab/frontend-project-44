90830dafa07f6b5b67e08d36b02598bde503982a
vlad-bab <118886918+vlad-bab@users.noreply.github.com>
2023-05-15 15:30:11 +0500

    Update cli.js

--- src/cli.js
+++ src/cli.js
@@ -4,7 +4,6 @@ export const welcomeUser = () => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
-  return userName;
 };
 
 export default welcomeUser;
